b408c5a60aed479f9178d4cc80927137b52d253c
xzh <ss612270623@gmail.com>
2023-12-09 16:23:16 +0800

    clean

--- README.md
+++
@@ -1,4 +0,0 @@
-# StonePet-Haven
-# remote test
-# 1130backend update
-# 1207 整合 by hsiao
--- users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('homepage/', views.homepage, name='homepage'),
-    path('login/', views.login, name='login'),
-    path('register/', views.register, name='register'),
-] 
