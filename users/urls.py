0b4da967f5963edeccdf65d38a23e8f68c037295
yves4416@gmail.com <yves@example.com>
2023-12-11 21:35:35 +0800

    register store to database except not avaliable value

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('register/', views.register, name='register'),
     path('forgot_password/', views.forgot_password, name='forgot_password'),
-
+    path('creat_account/', views.creat_account, name='creat_account'),
 ] 
