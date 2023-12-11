a5a84f7af5fac3321f2f1144d6aa5e7448f71414
HYHsiao0625 <as12345521117@gmail.com>
2023-12-11 14:50:02 +0800

    1211

--- users/views.py
+++ users/views.py
@@ -1,30 +1,39 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib import messages
-from django.shortcuts import render, redirect
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Product, User
+from .forms import LoginForm
 # Create your views here.
-def homepage(request):
-    template = loader.get_template('homepage.html')
-    context = {
+def home(request):
+    return render(request, 'home.html')
 
-    }
-    return HttpResponse(template.render(context, request))
 
 def login(request):
-    template = loader.get_template('login.html')
-    context = {
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            account = form.cleaned_data['account']
+            password = form.cleaned_data['password']
 
-    }
-    return HttpResponse(template.render(context, request))
+            user = User.objects.filter(account=account, password=password).first()
+
+            if user:
+                # 登入成功，重定向到主頁
+                return redirect('home')
+            else:
+                # 登入失敗，設定錯誤訊息
+                error = "Invalid username or password. Please try again."
+                return render(request, 'login.html', {'form': form, 'error': error})
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
 
 def register(request):
-    if request.method == 'POST':
-        # 处理用户注册逻辑
-        # 注册成功后，添加注册成功消息
-        messages.success(request, '注册成功！请登录。')
-        return redirect('login')
-    else:
-        # 显示注册页面
-        return render(request, 'register.html')
+    return render(request, 'register.html')
+
+def forgot_password(request):
+    return render(request, 'forgot_password.html')
--- web_development/settings.py
+++ web_development/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -127,7 +128,13 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 STATIC_URL = 'static/'
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = ['web_development.backends.CustomBackend']
